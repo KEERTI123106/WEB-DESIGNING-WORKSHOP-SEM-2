7abdfcd74bd773e838bfb07f32a63f1bfd529a60
Keerti Sharma <keerti1206@gmail.com>
2026-03-11 00:00:33 +0530

    added new styles

---
+++ pattern.js
@@ -0,0 +1,26 @@
+/* let x;                        SIMPLE PYRAMID
+for(x=1; x<=5; x++){
+        console.log("*".repeat(x));
+    }
+
+
+
+   let x;                         INVERTED PYRAMID
+   for(x=5 ; x>=1 ; x--){
+    console.log("*".repeat(x));
+   }
+
+
+
+  let i;                          FLIPPED SIMPLE PYRAMID
+  for(i=1;i<=5;i++){
+     console.log(' '.repeat(5-i) + '*'.repeat(i));
+  } 
+     
+
+  let i;                          FLIPPED INVERTED PYRAMID
+  for(i=5; i>=1; i--){
+    console.log( ' '.repeat(5-i) + '*'.repeat(i));
+  } */
+
+  
